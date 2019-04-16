82916ae6f9d1b88820f8b0b07b43daeddb3d78a0
mrc <marcoambrosini@pm.me>
2019-04-16 15:46:46 +0200

    Updates

---
+++ precache-manifest.fa4fcebcf3fd50df92f242ef47961484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeb472cacd64b1860715",
+    "url": "/calculator/static/css/main.d1f16859.chunk.css"
+  },
+  {
+    "revision": "eeb472cacd64b1860715",
+    "url": "/calculator/static/js/main.a798a981.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "f39c6945a72beaaea9df",
+    "url": "/calculator/static/js/2.7a19e083.chunk.js"
+  },
+  {
+    "revision": "d61d205496c920e7c69206cec37ba53e",
+    "url": "/calculator/index.html"
+  }
+];
